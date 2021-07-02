2079ad3964ea161e0b0945fecce89290df268041
Hannah Newsome <h.e.newsome@gmail.com>
2021-07-02 10:33:08 -0400

    switching to mongodb & hooking it up to laravel sail

--- config/database.php
+++ config/database.php
@@ -62,6 +62,16 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'options' => [
+                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'),
+            ]],
 
         'pgsql' => [
             'driver' => 'pgsql',
