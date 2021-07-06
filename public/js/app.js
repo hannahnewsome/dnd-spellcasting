5b14ad2072da7a12a22d74f707647554408ca567
Hannah Newsome <h.e.newsome@gmail.com>
2021-07-06 14:44:51 -0400

    extending js number prototype to add ordinal suffix function

--- public/js/app.js
+++ public/js/app.js
@@ -1987,6 +1987,38 @@ var app = new vue__WEBPACK_IMPORTED_MODULE_0__.default({
   el: '#app'
 });
 
+Number.prototype.ordinal = function () {
+  var num = this,
+      last = num.toString().slice(-1),
+      ord = '';
+
+  switch (last) {
+    case '1':
+      ord = 'st';
+      break;
+
+    case '2':
+      ord = 'nd';
+      break;
+
+    case '3':
+      ord = 'rd';
+      break;
+
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    case '0':
+      ord = 'th';
+      break;
+  }
+
+  return num.toString() + ord;
+};
+
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
--- resources/js/app.js
+++ resources/js/app.js
@@ -33,3 +33,30 @@ Vue.component('app', require('./views/app.vue').default);
 const app = new Vue({
     el: '#app',
 });
+
+Number.prototype.ordinal = function(){
+    var num = this,
+        last = num.toString().slice(-1),
+        ord = '';
+    switch (last) {
+        case '1':
+            ord = 'st';
+            break;
+        case '2':
+            ord = 'nd';
+            break;
+        case '3':
+            ord = 'rd';
+            break;
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '0':
+            ord = 'th';
+            break;
+    }
+    return num.toString() + ord;
+};
